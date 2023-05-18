86bd487951380b3eb75fcb5b48a2e4e078c0eb92
KhadizaSamiha <khadizasamiha7@gmail.com>
2023-05-18 21:10:21 +0600

    errorpage done

---
+++ src/Pages/Error.jsx
@@ -0,0 +1,13 @@
+
+
+const Error = () => {
+    return (
+        <div>
+            <div className="h-[680px]">
+                <img className="h-full w-full" src="https://i.ibb.co/MnQpxM5/2725947.jpg" alt="" />
+            </div>
+        </div>
+    );
+};
+
+export default Error;
--- src/main.jsx
+++ src/main.jsx
@@ -5,11 +5,13 @@ import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
+import Error from './Pages/Error';
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <div>Hello world!</div>,
+    errorElement: <Error></Error>
   },
 ]);
 
